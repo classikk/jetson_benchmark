06afc3a1221337dbdcae095f1cc1b5c7ed372547
classikk <lassi04kokkonen@gmail.com>
2026-03-24 13:32:39 +0200

    versions docker

--- init_docker.sh
+++ init_docker.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-docker pull nvidia/cuda:13.0.0-devel-ubuntu22.04
+is_jetson() {
+    local soc
+    soc=$(tr -d '\0' < /proc/device-tree/compatible 2>/dev/null)
+
+    if echo "$soc" | grep -qi "nvidia,tegra"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+if is_jetson; then
+    echo "This device is an NVIDIA Jetson"
+    version="nvcr.io/nvidia/l4t-cuda:12.2.12-runtime"
+    docker pull $version
+    docker tag $version compiler-base-base:latest
+else
+    echo "This device is NOT a Jetson"
+    echo "Gambling to guess nvidia version"
+    version="nvidia/cuda:$(nvidia-smi | grep "CUDA Version" | sed 's/.*CUDA Version: \([0-9.]*\).*/\1/')-devel-ubuntu22.04"
+    docker pull $version
+    docker tag $version compiler-base-base:latest
+fi
+
+#
+#
