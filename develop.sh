d9842d7dfc32b1d3404bd35243883e26c99e2860
classikk <lassi04kokkonen@gmail.com>
2026-03-30 21:08:23 +0300

    device options

--- develop.sh
+++ develop.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
-docker build -f DockerFile --target main -t jetson:main .
+docker build -f DockerFile --target main -t jetson_benchmark:main .
 
 # Main:
-docker run --rm -it --volume ./:/src -e DISPLAY=$DISPLAY  -v /tmp/.X11-unix:/tmp/.X11-unix jetson:main 
+xhost +local:docker
+
+DEVICES=""
+
+[ -e /dev/video0 ] && DEVICES="$DEVICES --device=/dev/video0"
+[ -e /dev/video1 ] && DEVICES="$DEVICES --device=/dev/video1"
+
+docker run --rm -it $DEVICES  --volume ./:/src -e DISPLAY=$DISPLAY  -v /tmp/.X11-unix:/tmp/.X11-unix jetson_benchmark:main 
 #docker run --rm --gpus all img:main
 
 # Debug:
