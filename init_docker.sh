d9611dc0fdeb5f31ca90e6dac1361275db460ef3
classikk <lassi04kokkonen@gmail.com>
2026-03-30 21:16:56 +0300

    user permission

--- init_docker.sh
+++ init_docker.sh
@@ -11,19 +11,25 @@ is_jetson() {
     fi
 }
 
-
 if is_jetson; then
     echo "This device is an NVIDIA Jetson"
     version="nvcr.io/nvidia/l4t-cuda:12.2.12-runtime"
     docker pull $version
     docker tag $version compiler-base-base:latest
 else
-    echo "This device is NOT a Jetson"
+    echo "This device is not NVIDIA Jetson"
     echo "Gambling to guess nvidia version"
     version="nvidia/cuda:$(nvidia-smi | grep "CUDA Version" | sed 's/.*CUDA Version: \([0-9.]*\).*/\1/')-devel-ubuntu22.04"
+    echo "This version may or may not be the correct docker image for this device:"
+    echo $version
+    read -p "Do you want to try pulling it? [y/N] " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "please pull docker image compatable with your device and name it as compiler-base-base:latest"
+        echo "docker pull {yourimage}"
+        echo "docker tag  {yourimage} compiler-base-base:latest"
+        exit 1
+    fi
     docker pull $version
     docker tag $version compiler-base-base:latest
 fi
-
-#
-#
